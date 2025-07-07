91417d8e892d470dd47b2564d4f35cc90610b5e1
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-07-07 15:44:04 +0200

    edit: added correct fonts + h1 title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DefaultLayout from "./layouts/defaultLayout";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DefaultLayout from './layouts/defaultLayout';
+import Home from './pages/Home';
+import Test from './pages/Test';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<DefaultLayout />}>
+        <Route path='/' element={<DefaultLayout />}>
+          <Route path='/test' element={<Test />} />
           <Route index element={<Home />} />
         </Route>
       </Routes>
