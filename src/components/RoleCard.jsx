bc08477843bea1ab4cbee252b41fe0d3d460f20c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-07-08 11:45:44 +0200

    add: show neutral background color roles

--- src/components/RoleCard.jsx
+++ src/components/RoleCard.jsx
@@ -4,27 +4,31 @@ export default function RoleCard({
   bgColor,
   onColor,
   bgRole = 'solid',
-  onRole = 'on solid',
+  onRole,
   colorClassname,
 }) {
   return (
     <>
       <div className='flex w-full flex-col items-center gap-2'>
         {/* on color text */}
-        <Tag color={onColor} />
+        {onColor && <Tag color={onColor} />}
         {/* bg color */}
         <div
-          className={`flex aspect-video min-h-20 w-full flex-col justify-between rounded-lg p-2 ${colorClassname}`}
+          className={`flex aspect-video min-h-20 w-full flex-col rounded-lg p-2 ${onColor ? 'justify-between' : 'justify-end'} ${colorClassname}`}
           style={{ backgroundColor: `${bgColor}` }}>
           {/* on color */}
-          <div
-            className={`flex aspect-video max-h-20 w-full flex-col items-center rounded-md p-2 ${colorClassname}`}
-            style={{ backgroundColor: `${onColor}` }}>
-            {/* on color role */}
-            <span className='font-body-m-bold' style={{ color: `${bgColor}` }}>
-              {onRole}
-            </span>
-          </div>
+          {onColor && (
+            <div
+              className={`flex aspect-video max-h-20 w-full flex-col items-center rounded-md p-2 ${colorClassname}`}
+              style={{ backgroundColor: `${onColor}` }}>
+              {/* on color role */}
+              <span
+                className='font-body-m-bold'
+                style={{ color: `${bgColor}` }}>
+                {onRole}
+              </span>
+            </div>
+          )}
           {/* bg color role */}
           <span
             className='font-body-m-bold self-center'
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -61,6 +61,21 @@ export default function Test() {
             />
           </div>
         </div>
+        {/* Neutral Color Roles ------------- */}
+        <div className='bg-neutral-base-300 border-neutral-border/20 col-span-full flex flex-col gap-4 rounded-xl border p-4 shadow-xl shadow-black/5'>
+          <h2 className='font-h4 text-center'>Ruoli Colore Neutrale</h2>
+          <div className='flex flex-col gap-10 md:max-h-60 md:flex-row'>
+            <RoleCard bgColor={neutralRoles?.background} bgRole='background' />
+            <RoleCard
+              bgColor={neutralRoles['background alt 1']}
+              bgRole='background alt 1'
+            />
+            <RoleCard
+              bgColor={neutralRoles['background alt 2']}
+              bgRole='background alt 2'
+            />
+          </div>
+        </div>
       </div>
     </>
   );
